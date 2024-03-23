4a499840429a525176ef41902fa619d927c88a68
nikhilnigamnik <nikhilnigamnik@gmail.com>
2024-03-23 16:33:32 +0530

    refactor(project) : redesign project layout

--- components/ProjectComponents/ProjectCard.jsx
+++ components/ProjectComponents/ProjectCard.jsx
@@ -2,29 +2,59 @@ import React from "react";
 import { CardSpotlight } from "../CardSpotlight";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
+import { CiMedicalClipboard } from "react-icons/ci";
+import { useCopyToClipboard } from "usehooks-ts";
+import { toast } from "sonner";
+import Link from "next/link";
 
 const ProjectCard = ({ data }) => {
+  const [copiedText, copy] = useCopyToClipboard();
+
+  const handleCopy = (text) => {
+    copy(text);
+    toast("Copied : " + copiedText, {
+      type: "success",
+    });
+  };
   return (
     <div className="flex animate_in  flex-col gap-4">
       {data.map((el) => (
         <CardSpotlight className={"lg:w-4/6 w-full"} key={el?._id} hoverEffect>
-          <div className="flex items-center gap-4">
-            <Image
-              src={el?.user?.image}
-              alt="avatar"
-              height={30}
-              width={30}
-              className="rounded-full"
-            />
-            <p>{el?.user?.name}</p>
+          <div className="flex items-center gap-4 justify-between">
+            <div className="flex items-center gap-4 ">
+              <Image
+                src={el?.user?.image}
+                alt="avatar"
+                height={30}
+                width={30}
+                className="rounded-full"
+              />
+              <p>{el?.user?.name}</p>
+            </div>
+            <div className="flex items-center gap-4 ">
+              <Link href={el?.repoLink}>
+                <Badge>Repo</Badge>
+              </Link>
+              <Link href={el?.projectLink}>
+                <Badge>Visit</Badge>
+              </Link>
+            </div>
           </div>
+
           <p className="text-lg font-bold text-gradient">{el?.title}</p>
           <p className="text-gray-200">{el?.description}</p>
-          <p className="text-gray-200">{el?.link}</p>
-          <div className="flex gap-3">
-            {el?.tags.map((tag, index) => (
-              <Badge key={index}>{tag}</Badge>
-            ))}
+          <div className="flex justify-between items-center">
+            <div className="flex gap-3 ">
+              {el?.tags.map((tag, index) => (
+                <Badge key={index}>{tag}</Badge>
+              ))}
+            </div>
+
+            <CiMedicalClipboard
+              className="cursor-pointer text-border"
+              onClick={() => handleCopy(el?.repoLink)}
+              size={24}
+            />
           </div>
         </CardSpotlight>
       ))}
--- components/ui/dialog.jsx
+++ components/ui/dialog.jsx
@@ -18,7 +18,7 @@ const DialogOverlay = React.forwardRef(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-50 backdrop-blur-xl  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
     {...props} />
@@ -31,15 +31,14 @@ const DialogContent = React.forwardRef(({ className, children, ...props }, ref) 
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-slate-200 bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:border-slate-800 dark:bg-slate-950",
+        "fixed rounded-xl left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 bg-secondary border-border border p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] ",
         className
       )}
       {...props}>
       {children}
       <DialogPrimitive.Close
-        className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 data-[state=open]:text-slate-500 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-slate-800 dark:data-[state=open]:text-slate-400">
-        <Cross2Icon className="h-4 w-4" />
-        <span className="sr-only">Close</span>
+        className="absolute right-4 top-4 rounded-md text-white  border border-border transition-opacity   data-[state=open]:bg-slate-100 data-[state=open]:text-slate-500 dark:ring-offset-slate-950 dark:focus:ring-slate-300 dark:data-[state=open]:bg-slate-800 dark:data-[state=open]:text-slate-400">
+        <Cross2Icon className="h-5 w-5" />
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
