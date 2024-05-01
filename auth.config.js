34daed643dbd8f3dce103bc9b6a6de5827a6db7a
nikhilnigamnik <nikhilnigamnik@gmail.com>
2024-05-01 19:59:49 +0530

    fix : auth while signup with google

--- auth.config.js
+++ auth.config.js
@@ -6,6 +6,7 @@ export default {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
       profile(profile) {
         return {
           id: profile.id,
